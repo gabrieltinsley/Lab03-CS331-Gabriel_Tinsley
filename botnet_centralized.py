a836646a91e7bb1cef66445a61d4a95a89aed830
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2025-04-20 02:03:55 -0600

    Create botnet_centralized.py

---
+++ botnet_centralized.py
@@ -0,0 +1,3 @@
+# Author: Gabriel Tinsley
+# Date: 4/20/2025
+# Description: Creates a central C&C server connecting bots to the server, and the C&C sends commands to all bots, and logs command delivery.
